c904b5064d81e901ad8483d3f360a48f6e4786d9
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-12-05 18:25:00 +0900

    Initialize project

---
+++ awjntr/awjntr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  awjntr
+//
+//  Created by tomoya tanaka on 2020/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
